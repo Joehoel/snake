4cd8b21bead33f33fc8a47012ca1b27762b11f0e
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-04-25 12:05:41 +0200

    Cleaned code

---
+++ Snake.js
@@ -0,0 +1,88 @@
+class Snake {
+  constructor() {
+    this.x = (Math.floor(Math.random() * grid.rows - 1) + 1) * scale;
+    this.y = (Math.floor(Math.random() * grid.columns - 1) + 1) * scale;
+
+    this.xs = scale * 1;
+    this.ys = 0;
+
+    this.total = 0;
+    this.tail = [];
+  }
+
+  draw() {
+    ctx.fillStyle = "#FFFFFF";
+    for (let i = 0; i < this.tail.length; i++) {
+      ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+    }
+    ctx.fillRect(this.x, this.y, scale, scale);
+  }
+
+  update() {
+    for (let i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
+    }
+
+    this.tail[this.total - 1] = { x: this.x, y: this.y };
+
+    this.x += this.xs;
+    this.y += this.ys;
+
+    if (this.x >= canvas.width) {
+      this.x = 0;
+    } else if (this.x < 0) {
+      this.x = canvas.width;
+    } else if (this.y >= canvas.height) {
+      this.y = 0;
+    } else if (this.y < 0) {
+      this.y = canvas.height;
+    }
+  }
+
+  eat(fruit) {
+    if (this.x == fruit.x && this.y == fruit.y) {
+      this.total++;
+      return true;
+    }
+
+    return false;
+  }
+
+  check() {
+    for (let i = 0; i < this.tail.length; i++) {
+      if (this.x == this.tail[i].x && this.y == this.tail[i].y) {
+        Storage.setHighscore(this.total);
+        updateHighscore(currentUser.uid);
+        this.total = 0;
+        this.tail = [];
+        clearInterval(loop);
+        started = false;
+        message.textContent = "You died, press any key to try again...";
+      }
+    }
+  }
+
+  steer(direction) {
+    switch (direction) {
+      case "up":
+        this.xs = 0;
+        this.ys = -scale;
+        break;
+      case "right":
+        this.xs = scale;
+        this.ys = 0;
+        break;
+      case "down":
+        this.xs = 0;
+        this.ys = scale;
+        break;
+      case "left":
+        this.xs = -scale;
+        this.ys = 0;
+        break;
+
+      default:
+        break;
+    }
+  }
+}
